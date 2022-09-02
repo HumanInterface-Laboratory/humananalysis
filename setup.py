9be348c05bcd94b017a20cb0b71f70eff6a891a1
kyosukemine <k3.mine.0131@gmail.com>
2022-09-02 10:10:50 +0900

    3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='humananalysis',
-    version="0.1.2",                 # X.Y.Z 形式
+    version="0.1.3",                 # X.Y.Z 形式
     description="短めの説明",
     long_description="長めの説明",
     url='https://github.com/kyosukemine/humananalysis',
